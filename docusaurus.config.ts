9aeb3baf9d5c80ec000bae8be207dac134dd7050
Rodrigo Gomes <rodrigo.dev.ux@outlook.com>
2026-04-12 21:08:20 -0300

    fix(i18n): remove "en" locale on configuration file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'pt-br',
-    locales: ['pt-br', 'en'],
+    locales: ['pt-br'],
   },
 
   presets: [
